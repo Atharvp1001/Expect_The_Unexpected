a556256248adcc36e3868f3947b079af1ca197f1
Sarbojit Mandal <sarbojit2424@gmail.com>
2025-01-26 12:18:44 +0530

    Outcome

--- Expect_The_Unexpected/Assets/Scripts/OutcomeManager.cs
+++ Expect_The_Unexpected/Assets/Scripts/OutcomeManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OutcomeManager : MonoBehaviour
 {
@@ -15,4 +16,26 @@ public class OutcomeManager : MonoBehaviour
             Debug.LogError("Panel is not assigned in the Inspector!");
         }
     }
+
+    public void retry() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void next() 
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        // Check if the next scene index is valid
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.Log("No more scenes to load. You're at the last scene.");
+        }
+    }
+
 }
